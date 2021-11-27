ab8b8bfbced955556c406e675e7b38747b8f5a7e
wataru shinzato <xinlishe@gmail.com>
2021-11-27 23:46:52 +0900

    wataru edit

--- src/chbs/CHBS.java
+++ src/chbs/CHBS.java
@@ -14,5 +14,6 @@ public class CHBS {
         System.out.println("x");
         System.out.println("nagatomo writed");
         System.out.println("wataru writed");
+        System.out.println("wataru writed");
     }
 }
