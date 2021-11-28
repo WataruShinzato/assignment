b8619fe08e7fe4079cf94f85a71e13f88d9b7f25
nagatomo <enu1996502@gmail.com>
2021-11-28 16:50:29 +0900

    committee booking searching completed

--- src/chbs/CommitteeBookingManage.java
+++ src/chbs/CommitteeBookingManage.java
@@ -3,7 +3,6 @@ package chbs;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import java.awt.Button;
 import java.awt.FlowLayout;
@@ -35,13 +34,12 @@ public class CommitteeBookingManage extends JFrame implements ActionListener {
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == addBooking) {
       String inputName = JOptionPane.showInputDialog("Username:");
-      int inputPassword = Integer.parseInt(JOptionPane.showInputDialog("Password:"));
-      //MyCustomer customer = new MyCustomer(inputName, inputPassword);
+      // TODO: passwordも入力させる
       MyCustomer customer = DataIO.checking(inputName);
-      if(customer!= null){
+      if (customer != null) {
         int size = customer.getMyBookings().size();
-        
-        if(size == 0 || customer.getMyBookings().get(size - 1).isPaid()) {
+
+        if (size == 0 || customer.getMyBookings().get(size - 1).isPaid()) {
           try {
             Consultant a = Consultant.valueOf(JOptionPane.showInputDialog("Consultant:"));
             Day b = Day.valueOf(JOptionPane.showInputDialog("Day:"));
@@ -72,46 +70,50 @@ public class CommitteeBookingManage extends JFrame implements ActionListener {
         } else {
           JOptionPane.showMessageDialog(addBooking, "You have unpaid booking!");
         }
-      } else{
+      } else {
         JOptionPane.showMessageDialog(addBooking, "There is no name in the booking!");
       }
     } else if (e.getSource() == cancelBooking) {
       String inputName = JOptionPane.showInputDialog("Username :");
-      // MyCustomer customer = new MyCustomer(inputName, inputPassword);
-      // for (Booking userBooking : DataIO.allBookings) {
-      // if (userBooking.getOnwer().getName().equals(inputName)) {
-      // userBook = userBooking;
-      // break;
-      // }
-      // userBook = null;
-      // }
       MyCustomer SearchUser = DataIO.checking(inputName);
       System.out.println(SearchUser.getMyBookings().size());
-      if(SearchUser.getMyBookings().size()>0){
+      if (SearchUser.getMyBookings().size() > 0) {
         Booking userBooking = SearchUser.getMyBookings().get(0);
-        if(DataIO.allBookings.remove(userBooking) && SearchUser.getMyBookings().remove(userBooking)){
+        if (DataIO.allBookings.remove(userBooking) && SearchUser.getMyBookings().remove(userBooking)) {
           DataIO.write();
           JOptionPane.showMessageDialog(cancelBooking, "You removed reservation!");
-        }else{
+        } else {
           JOptionPane.showMessageDialog(cancelBooking, "There's no reservation");
         }
       }
-      /*
-       * Booking userbBooking = customer.getMyBookings().get(0);
-       * 
-       * 
-       * if (customer.getMyBookings().size()>0 &&
-       * customer.getMyBookings().remove(userbBooking)) {
-       * DataIO.write();
-       * JOptionPane.showMessageDialog(cancelBooking, "You removed reservation!");
-       * } else {
-       * JOptionPane.showMessageDialog(cancelBooking, "There's no reservation");
-       * }
-       */
     } else if (e.getSource() == modifyBooking) {
       // TODO
     } else if (e.getSource() == searchBooking) {
-      // TODO
+      String inputName = JOptionPane.showInputDialog("Username :");
+      MyCustomer searchUser = DataIO.checking(inputName);
+
+      if (searchUser == null) {
+        JOptionPane.showMessageDialog(searchBooking, "There's no user in customer");
+        return;
+      }
+
+      int inputPassword = Integer.parseInt(JOptionPane.showInputDialog("Password:"));
+
+      if (searchUser.getPassword() != inputPassword) {
+        JOptionPane.showMessageDialog(searchBooking, "Wrong password");
+        return;
+      }
+
+      Booking searchUserBooking = searchUser.getMyBookings().get(0);
+
+      String bookingId = String.valueOf(searchUserBooking.getId());
+      String bookingConsultant = searchUserBooking.getConsultant().toString();
+      String bookingDay = searchUserBooking.getDay().toString();
+      String bookingTime = String.valueOf(searchUserBooking.getTime());
+
+      String dialogLabel = bookingId + "\n" + bookingConsultant + "\n" + bookingDay + "\n" + bookingTime + "\n"
+          + "Register: " + inputName;
+      JOptionPane.showMessageDialog(searchBooking, dialogLabel);
     }
   }
 }
