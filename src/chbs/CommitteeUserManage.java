b8283d5f7794322c06e88f626e43799fc6d86709
nagatomo <enu1996502@gmail.com>
2021-11-29 21:11:28 +0900

    delete user by committee completed

--- src/chbs/CommitteeUserManage.java
+++ src/chbs/CommitteeUserManage.java
@@ -8,7 +8,7 @@ import java.awt.Button;
 import java.awt.FlowLayout;
 
 public class CommitteeUserManage extends JFrame implements ActionListener {
-  private Button addUserAccount, cancelUserAccount, modifyUserAccount, searchUserAccount;
+  private Button addUserAccount, deleteUserAccount, modifyUserAccount, searchUserAccount;
 
   public CommitteeUserManage() {
     setSize(500, 250);
@@ -16,17 +16,17 @@ public class CommitteeUserManage extends JFrame implements ActionListener {
     setLayout(new FlowLayout());
 
     addUserAccount = new Button("Add UserAccount");
-    cancelUserAccount = new Button("Delete UserAccount");
+    deleteUserAccount = new Button("Delete UserAccount");
     modifyUserAccount = new Button("Edit UserAccount");
     searchUserAccount = new Button("Search UserAccount");
 
     addUserAccount.addActionListener(this);
-    cancelUserAccount.addActionListener(this);
+    deleteUserAccount.addActionListener(this);
     modifyUserAccount.addActionListener(this);
     searchUserAccount.addActionListener(this);
 
     add(addUserAccount);
-    add(cancelUserAccount);
+    add(deleteUserAccount);
     add(modifyUserAccount);
     add(searchUserAccount);
   }
@@ -44,33 +44,43 @@ public class CommitteeUserManage extends JFrame implements ActionListener {
       } else {
         JOptionPane.showMessageDialog(addUserAccount, "The username has been used!");
       }
-    } else if (e.getSource() == cancelUserAccount) {
+    } else if (e.getSource() == deleteUserAccount) {
       String inputName = JOptionPane.showInputDialog("Username :");
-
-      Booking userBook = null;
-      for (Booking userBooking : DataIO.allBookings) {
-        if (userBooking.getOnwer().getName().equals(inputName)) {
-          userBook = userBooking;
-        }
+      MyCustomer found = DataIO.checking(inputName);
+      if (found == null) {
+        JOptionPane.showMessageDialog(deleteUserAccount, "There is no name");
+        return;
       }
-      System.out.println(userBook);
-    } else if (e.getSource() == modifyUserAccount) {
-            
-            String inputName = String.valueOf(JOptionPane.showInputDialog("Name:")); // reinput name from user
-            MyCustomer found = DataIO.checking(inputName);
-            if (found !=null){
-              int indexOfMyInfo = DataIO.allCustomers.indexOf(found);
-              String inputName2 = String.valueOf(JOptionPane.showInputDialog("New Name:"));
-              int password = Integer.parseInt(JOptionPane.showInputDialog("New Password:")); // reinput password from user
-              MyCustomer updatedMyData = new MyCustomer(inputName2, password); // assign inputname and password to
 
-              found.setName(inputName2); // set inputname
-              found.setPassword(password); // set password
-              DataIO.allCustomers.set(indexOfMyInfo, updatedMyData); // replace the data before and after the change.
-              DataIO.write(); // write it on people file
-            }else{
-              JOptionPane.showMessageDialog(modifyUserAccount,"There is no name");
-            }
+      int inputPassword = Integer.parseInt(JOptionPane.showInputDialog("Password:"));
+      if (found.getPassword() != inputPassword) {
+        JOptionPane.showMessageDialog(deleteUserAccount, "Wrong password");
+        return;
+      }
+
+      if (DataIO.allCustomers.remove(found)) {
+        DataIO.write();
+        JOptionPane.showMessageDialog(deleteUserAccount, inputName + "'s account has been deleted");
+      } else {
+        System.out.println("failed to delete");
+      }
+    } else if (e.getSource() == modifyUserAccount) {
+
+      String inputName = String.valueOf(JOptionPane.showInputDialog("Name:")); // reinput name from user
+      MyCustomer found = DataIO.checking(inputName);
+      if (found != null) {
+        int indexOfMyInfo = DataIO.allCustomers.indexOf(found);
+        String inputName2 = String.valueOf(JOptionPane.showInputDialog("New Name:"));
+        int password = Integer.parseInt(JOptionPane.showInputDialog("New Password:")); // reinput password from user
+        MyCustomer updatedMyData = new MyCustomer(inputName2, password); // assign inputname and password to
+
+        found.setName(inputName2); // set inputname
+        found.setPassword(password); // set password
+        DataIO.allCustomers.set(indexOfMyInfo, updatedMyData); // replace the data before and after the change.
+        DataIO.write(); // write it on people file
+      } else {
+        JOptionPane.showMessageDialog(modifyUserAccount, "There is no name");
+      }
     } else if (e.getSource() == searchUserAccount) {
       // TODO
     }
