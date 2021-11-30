3efad11f4ddc5932c03c00ccb8d35dd6cb1b5fa9
nagatomo <enu1996502@gmail.com>
2021-11-30 22:26:00 +0900

    一旦コミット

--- src/chbs/StatisticalReport.java
+++ src/chbs/StatisticalReport.java
@@ -1,6 +1,38 @@
 
 package chbs;
 
-public class StatisticalReport {
+import java.awt.event.*;
 
+import javax.swing.JOptionPane;
+
+import java.awt.Button;
+
+public class StatisticalReport {
+    private Button searchByAge;
+
+    public StatisticalReport() {
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == searchByAge) {
+            String data = "";
+            // 下のやつを一行ずつ String にまとめてそこに出す
+            // 10代何名？
+            // 20代何名？
+            // 30代何名？
+            // 40代何名？
+            for (int i = 0; i < 8; i++) {
+
+            }
+            JOptionPane.showMessageDialog(searchByAge, data);
+        } else if (e.getSource() == searchByAge) {
+            String data = "";
+            // TODO: 男性の人数を定義
+            JOptionPane.showMessageDialog(searchByAge, data);
+        } else if (e.getSource() == searchByAge) {
+            String data = "";
+            // TODO: 女性の人数を定義
+            JOptionPane.showMessageDialog(searchByAge, data);
+        }
+    }
 }
